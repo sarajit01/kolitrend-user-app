55d42c14d010c5d0f236705c59df93d786de33be
Sarajit <sarajitbarmon3@gmail.com>
2025-09-04 15:49:42 +0600

    User app without custom changes

---
+++ lib/features/home/widgets/product_list_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sixvalley_ecommerce/common/basewidget/no_internet_screen_widget.dart';
+import 'package:flutter_sixvalley_ecommerce/common/basewidget/paginated_list_view_widget.dart';
+import 'package:flutter_sixvalley_ecommerce/common/basewidget/product_shimmer_widget.dart';
+import 'package:flutter_sixvalley_ecommerce/common/basewidget/product_widget.dart';
+import 'package:flutter_sixvalley_ecommerce/features/product/controllers/product_controller.dart';
+import 'package:flutter_sixvalley_ecommerce/helper/responsive_helper.dart';
+import 'package:flutter_sixvalley_ecommerce/utill/dimensions.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
+
+class HomeProductListWidget extends StatelessWidget {
+  const HomeProductListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ProductController>(
+      builder: (context, productController, child) {
+        if (productController.selectedProductModel?.products?.isNotEmpty ?? false) {
+
+          return PaginatedListView(
+            onPaginate: (int? offset) async {
+              await productController.getSelectedProductModel(offset ?? 1);
+            },
+            totalSize: productController.selectedProductModel?.totalSize,
+            offset: productController.selectedProductModel?.offset,
+            limit: productController.selectedProductModel?.limit,
+            itemView: Expanded(child: MasonryGridView.count(
+              itemCount: productController.selectedProductModel?.products?.length ?? 0,
+              crossAxisCount: ResponsiveHelper.isTab(context) ? 3 : 2,
+              padding: const EdgeInsets.symmetric(horizontal: Dimensions.paddingSizeSmall),
+              itemBuilder: (BuildContext context, int index) {
+                return ProductWidget(productModel: productController.selectedProductModel!.products![index]);
+              },
+            )), // Replace with your actual item view
+          );
+
+        } else if (productController.selectedProductModel?.products?.isEmpty ?? false) {
+          return const NoInternetOrDataScreenWidget(isNoInternet: false);
+
+        } else {
+          return const ProductShimmer(isHomePage: false, isEnabled: true);
+        }
+      },
+    );
+  }
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,68 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_session
+import audioplayers_darwin
+import connectivity_plus
+import device_info_plus
+import emoji_picker_flutter
+import facebook_auth_desktop
+import file_selector_macos
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import flutter_inappwebview_macos
+import flutter_local_notifications
+import flutter_secure_storage_macos
+import geolocator_apple
+import google_sign_in_ios
+import just_audio
+import open_file_mac
+import package_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import sign_in_with_apple
+import sqflite_darwin
+import sqlite3_flutter_libs
+import syncfusion_pdfviewer_macos
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_plus
+import webview_flutter_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
+}
